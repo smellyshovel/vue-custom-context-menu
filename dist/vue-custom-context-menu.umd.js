b620203c539b39fabf4a0d25c3153f7db497cf7a
Matthew Mamonov <g.smellyshovel@gmail.com>
2018-12-18 18:28:05 +0300

    solved #3 by providing watchers added minifier for unpkg build

--- dist/vue-custom-context-menu.umd.js
+++ dist/vue-custom-context-menu.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    global.CCM = factory();
+    global.VCCM = factory();
 }(typeof self !== 'undefined' ? self : this, function () { 'use strict';
 
     var ContextMenuDirective = {
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
-// rollup.config.js
-import BabelPlugin from "rollup-plugin-babel";
 import VuePlugin from "rollup-plugin-vue";
+import BabelPlugin from "rollup-plugin-babel";
+import BabelMinifyPlugin from 'rollup-plugin-babel-minify';
 
 export default {
-    input: 'src/main.js',
+    input: "src/main.js",
+
     output: {
-        name: "CCM"
+        name: "VCCM"
     },
 
     plugins: [
-        BabelPlugin({
-            exclude: 'node_modules/**'
-        }),
         VuePlugin({
             css: true, // Dynamically inject css as a <style> tag
             compileTemplate: true, // Explicitly convert template to render function
         }),
+        BabelPlugin({
+            exclude: "node_modules/**"
+        }),
+        process.env.MINIFY ? BabelMinifyPlugin() : function() {}
     ]
 };
